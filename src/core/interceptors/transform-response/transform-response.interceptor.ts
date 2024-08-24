5b5836dfad1412a65e9eaf9ed1329126840b6a3d
Jimenez Juan <jimenez.f.juan.m@gmail.com>
2024-08-24 17:52:44 -0400

    Add TransformResponseInterceptor to standardize API responses
    - Implement TransformResponseInterceptor to normalize response format
    - Register TransformResponseInterceptor as a global interceptor in CoreModule

---
+++ src/core/interceptors/transform-response/transform-response.interceptor.ts
@@ -0,0 +1,34 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class TransformResponseInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map((response) => {
+        if (!response) {
+          return {
+            data: [],
+          };
+        }
+
+        if (response.data && response.meta) {
+          return {
+            data: response.data,
+            meta: response.meta,
+          };
+        }
+
+        return {
+          data: response,
+        };
+      }),
+    );
+  }
+}
