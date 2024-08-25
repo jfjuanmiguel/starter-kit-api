8fe54b9ce955a77221fa65d880ba67010982e3a5
Jimenez Juan <jimenez.f.juan.m@gmail.com>
2024-08-25 17:07:57 -0400

    Set up e2e testing environment with Docker and Jest
    - Add Docker Compose file for test environment (docker-compose-test.yml)
    - Update package.json with new scripts for test environment
    - Configure Jest for e2e tests (jest-e2e.json)
    - Implement global setup and teardown for e2e tests
    - Add database reset and cache clearing in test setup
    - Update .gitignore to exclude test-related files
    - Refactor core module and database service for better testability

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -11,7 +11,7 @@ import { APP_INTERCEPTOR } from '@nestjs/core';
 import { TransformResponseInterceptor } from './interceptors/transform-response/transform-response.interceptor';
 import { LoggerService } from './logger/logger.service';
 import { LoggerMiddleware } from './logger/logger.middleware';
-import { DatabaseModule } from 'src/database/database.module';
+import { DatabaseModule } from '../database/database.module';
 import { CacheModule } from '@nestjs/cache-manager';
 import * as redisStore from 'cache-manager-redis-store';
 import { CacheService } from './cache/cache.service';
