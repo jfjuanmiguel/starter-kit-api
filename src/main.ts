293b534a24457251fdc8d48cb94c94b6beb40ade
Jimenez Juan <jimenez.f.juan.m@gmail.com>
2024-08-27 11:25:55 -0400

    feat: configure dynamic port and host for server
    - Add support for PORT environment variable
    - Set default host to 0.0.0.0 for broader accessibility

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,8 @@ async function bootstrap() {
   app.useLogger(app.get(LoggerService));
   app.use(helmet());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(3000);
+  const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+  const host = `0.0.0.0`;
+  await app.listen(port, host);
 }
 bootstrap();
